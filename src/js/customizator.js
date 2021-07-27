c0c179659f31deb4f223921f55bbdacdc5f56ce0
yura <yura04091@gmail.com>
2021-07-27 22:57:33 +0300

    first commit

---
+++ src/js/customizator.js
@@ -0,0 +1,13 @@
+export default class Customizator {
+    constructor() {
+
+    }
+
+    render() {
+        this.btnBlock = document.createElement('div');
+        let scaleInputS = document.createElement('input'),
+            scaleInputM = document.createElement('input');
+        
+        console.log(this.btnBlock, scaleInputS, scaleInputM);
+    }   
+}
